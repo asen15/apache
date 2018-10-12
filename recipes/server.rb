c485f20ff49643e23044858f8d55792d831629ac
Cloud User <centos@ip-172-31-47-18.ec2.internal>
2018-10-12 21:14:32 +0000

    index.html updated with host information

--- recipes/server.rb
+++ recipes/server.rb
@@ -3,7 +3,15 @@ package 'httpd' do
 end
 
 file '/var/www/html/index.html' do
- content '<html><body><h1>Hello World !!!</h1></body></html>'
+ content "<html><body>
+ <h1>Welcome To My World !!!</h1><br>
+ <table>
+ <tr><td>HOSTNAME</td><td>#{node['hostname']}</td></tr>
+ <tr><td>IPADDRESS</td><td>#{node['ipaddress']}</td></tr>
+ <tr><td>CPU</td><td>#{node['cpu']['0']['mhz']}</td></tr>
+ <tr><td>Memory</td><td>#{node['memory']['total']}</td></tr>
+ </table>
+ </body></html>"
  action :create
  owner 'root'
  group 'root'
